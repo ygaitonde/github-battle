af966af10adb41a50befce0c4bddbb3416e70851
Yash Gaitonde <gaitoyas20@7hills.org>
2020-05-06 09:56:20 -0400

    netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './app/index.js',
@@ -17,7 +18,10 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'app/index.html'
-        })
+        }),
+        new CopyPlugin([
+            {from: "_redirects"}
+        ])
     ],
     mode: process.env.NODE_ENV==='production' ? 'production' : 'development',
     devServer: {
